24a2c880e51d4f645f8a191ca9fae8b1d04de0cb
Efosa Oyegun <efosao@gmail.com>
2019-04-08 08:16:16 -0700

    added semantic-ui for styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,32 @@
+import {
+  Container,
+  Grid,
+  Header,
+  Icon,
+  GridColumn
+} from 'semantic-ui-react'
+
 const Page = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>Etc ....</p>
-    </div>
+    <Grid>
+      <GridColumn>
+        <Container>
+          <Header as='h2'>
+            <Icon color='blue' name='linkify' />
+            <Header.Content>
+              Links Saver
+              <Header.Subheader>By Efosa Oyegun</Header.Subheader>
+            </Header.Content>
+          </Header>
+          <p>Etc ....</p>
+        </Container>
+      </GridColumn>
+      <style jsx global>{`
+        body {
+          margin: 20px 0 0;
+        }
+      `}</style>
+    </Grid>
   )
 }
 
