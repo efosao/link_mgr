2491d74ed85f09658d8b6a0a12134ce083a8386b
Efosa Oyegun <efosao@gmail.com>
2019-04-08 10:49:17 -0700

    added `baseUrl` to constants

--- lib_shared/constants.js
+++ lib_shared/constants.js
@@ -3,6 +3,7 @@ const isServer = typeof window === 'undefined'
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  baseUrl: 'http://localhost:3000',
   isDev: !isProd,
   isProd,
   isClient: !isServer,
